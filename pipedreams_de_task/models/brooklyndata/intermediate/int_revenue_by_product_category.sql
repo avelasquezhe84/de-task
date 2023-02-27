6cd9005c84b154cafc2455813fed6dbaf57a9c61
Andres Velasquez <andresvel00@gmail.com>
2023-02-27 09:13:23 -0500

    mart model

--- pipedreams_de_task/models/brooklyndata/intermediate/int_revenue_by_product_category.sql
+++ pipedreams_de_task/models/brooklyndata/intermediate/int_revenue_by_product_category.sql
@@ -1,3 +1,4 @@
+-- IMPORTS
 with orders as (
     select * from {{ ref('stg_snowflake__orders') }}
 )
@@ -10,7 +11,8 @@ with orders as (
     select * from {{ ref('stg_snowflake__products') }}
 )
 
-, final as (
+-- TRANSFORMATION
+, revenue_by_category as (
     select 
         o.order_purchase_date as purchase_date,
         p.product_category_name as category,
@@ -20,6 +22,22 @@ with orders as (
     join items i on o.order_id = i.order_id 
     join products p on i.product_id = p.product_id 
     group by purchase_date, category
+    order by purchase_date desc, rnk
+)
+
+-- FINAL SELECT
+, final as (
+    select
+        purchase_date,
+        max(case when rnk = 1 then category else null end) AS top_1_category,
+        max(case when rnk = 1 then revenue else null end) AS top_1_category_revenue,
+        max(case when rnk = 2 then category else null end) AS top_2_category,
+        max(case when rnk = 2 then revenue else null end) AS top_2_category_revenue,
+        max(case when rnk = 3 then category else null end) AS top_3_category,
+        max(case when rnk = 3 then revenue else null end) AS top_3_category_revenue
+    from revenue_by_category
+    group by purchase_date 
+    order by purchase_date desc
 )
 
 select * from final
--- pipedreams_de_task/models/brooklyndata/marts/mrt_order_report.sql
+++ pipedreams_de_task/models/brooklyndata/marts/mrt_order_report.sql
@@ -15,8 +15,12 @@ with orders as (
     select * from {{ ref('stg_snowflake__products') }}
 )
 
--- FINAL SELECT
-, final as (
+, revenue_by_category as (
+    select * from {{ ref('int_revenue_by_product_category') }}
+)
+
+-- TRANSFORMATION
+, total_revenue as (
     select
         o.order_purchase_date as purchase_date,
         count(distinct o.order_id) as total_orders,
@@ -25,7 +29,26 @@ with orders as (
         round((sum(p.payment_value) / count(distinct o.order_id)), 2) as average_revenue_per_order
     from orders o
     join payments p on o.order_id = p.order_id
+    where lower(trim(o.order_status)) <> 'canceled'
     group by purchase_date
+)
+
+-- FINAL SELECT
+, final as (
+    select
+        t.purchase_date,
+        t.total_orders,
+        t.total_customers_making_orders,
+        t.total_revenue,
+        t.average_revenue_per_order,
+        r.top_1_category,
+        (ifnull(r.top_1_category_revenue, 0) / total_revenue) * 100 as top_1_category_revenue_share,
+        r.top_2_category,
+        (ifnull(r.top_2_category_revenue, 0) / total_revenue) * 100 as top_2_category_revenue_share,
+        r.top_3_category,
+        (ifnull(r.top_3_category_revenue, 0) / total_revenue) * 100 as top_3_category_revenue_share        
+    from total_revenue t
+    join revenue_by_category r on t.purchase_date = r.purchase_date
     order by purchase_date desc
 )
 
