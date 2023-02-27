2a17effed21065eac6398bb41520ccbd0f22ced3
Andres Velasquez <andresvel00@gmail.com>
2023-02-27 10:06:59 -0500

    fix to final model

--- pipedreams_de_task/models/brooklyndata/marts/mrt_order_report.sql
+++ pipedreams_de_task/models/brooklyndata/marts/mrt_order_report.sql
@@ -7,19 +7,11 @@ with orders as (
     select * from {{ ref('stg_snowflake__order_payments') }}
 )
 
-, items as (
-    select * from {{ ref('stg_snowflake__order_items') }}
-)
-
-, products as (
-    select * from {{ ref('stg_snowflake__products') }}
-)
-
 , revenue_by_category as (
     select * from {{ ref('int_revenue_by_product_category') }}
 )
 
--- TRANSFORMATION
+-- TRANSFORMATIONS
 , total_revenue as (
     select
         o.order_purchase_date as purchase_date,
@@ -33,23 +25,35 @@ with orders as (
     group by purchase_date
 )
 
+, top_categories as (
+    select
+        t.purchase_date as order_purchase_date,
+        t.total_orders as orders_count,
+        round(t.total_customers_making_orders, 2) as customers_making_orders_count,
+        t.total_revenue as revenue_usd,
+        round(t.average_revenue_per_order, 2) as average_revenue_per_order_usd,
+        ifnull(r.top_1_category, '') as top_1_category,
+        round((ifnull(r.top_1_category_revenue, 0) / total_revenue) * 100, 2) as top_1_category_revenue_share,
+        ifnull(r.top_2_category, '') as top_2_category,
+        round((ifnull(r.top_2_category_revenue, 0) / total_revenue) * 100, 2) as top_2_category_revenue_share,
+        ifnull(r.top_3_category, '') as top_3_category,
+        round((ifnull(r.top_3_category_revenue, 0) / total_revenue) * 100, 2) as top_3_category_revenue_share        
+    from total_revenue t
+    join revenue_by_category r on t.purchase_date = r.purchase_date
+)
+
 -- FINAL SELECT
 , final as (
     select
-        t.purchase_date,
-        t.total_orders,
-        t.total_customers_making_orders,
-        t.total_revenue,
-        t.average_revenue_per_order,
-        r.top_1_category,
-        (ifnull(r.top_1_category_revenue, 0) / total_revenue) * 100 as top_1_category_revenue_share,
-        r.top_2_category,
-        (ifnull(r.top_2_category_revenue, 0) / total_revenue) * 100 as top_2_category_revenue_share,
-        r.top_3_category,
-        (ifnull(r.top_3_category_revenue, 0) / total_revenue) * 100 as top_3_category_revenue_share        
-    from total_revenue t
-    join revenue_by_category r on t.purchase_date = r.purchase_date
-    order by purchase_date desc
+        order_purchase_date,
+        orders_count,
+        customers_making_orders_count,
+        revenue_usd,
+        average_revenue_per_order_usd,
+        concat_ws(', ', top_1_category, top_2_category, top_3_category) as top_3_product_categories_by_revenue,
+        concat_ws(', ', top_1_category_revenue_share, top_2_category_revenue_share, top_3_category_revenue_share) as top_3_product_categories_revenue_percentage
+    from top_categories
+    order by order_purchase_date desc
 )
 
 select * from final
